5c1ba3f1e1c874226f31495f9cf24ea20cc3a861
UdarGIT829 <viraatudar9@gmail.com>
2024-04-10 23:09:45 -0700

    Switching to LiveShare

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 # MLP architecture
-class MLP(nn.Module):
+class MLP(nn.Module): # All models should inherit from nn.Module
     def __init__(self, num_input, hidden_size, num_classes):
         super(MLP, self).__init__()
+
+        #ALLGOOD
         self.fc1 = nn.Linear(num_input, hidden_size)
         self.fc2 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         x = torch.flatten(x, 1)
+
+        # WE NEED TO SWITCH THIS WITH AN APPROPRIATE ACTIVATION FUNCTION
         x = F.relu(self.fc1(x))
+        # ^^^
+        
         x = self.fc2(x)
         return x
 
